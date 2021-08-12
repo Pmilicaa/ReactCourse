efcba82723387452ec8c2c71bd2b44a569176e75
Pmilicaa <micy_98@live.com>
2021-08-12 03:22:26 +0200

    Added attr reminder and functions deleteTask and toggleReminder

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -5,17 +5,28 @@ function App() {
   const [tasks, setTasks] = useState([
     {
         id: 1,
-        text: 'See doctor'
+        text: 'See doctor',
+        reminder: true
     },
     {
         id:2,
-        text: 'Go to school'
+        text: 'Go to school',
+        reminder: true 
     }
 ])
+//delete task
+const deleteTask = (id)=>{
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
+//toggle reminder
+const toggleReminder = (id)=>{
+  setTasks(tasks.map((task)=>task.id ===id ? {...task, reminder : !task.reminder} : task)
+  )
+}
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/> : 'No Tasks to show'}
     </div>
   );
 }
